756f1c94f38db384f4640dc66f035e0e7a5e200f
sp4c3M4n <sanchez.carlos672@gmail.com>
2025-05-27 00:06:30 -0500

    added completed program with my refactoring

--- bullet_point_adder/bulletPointAdder.py
+++ bullet_point_adder/bulletPointAdder.py
@@ -2,6 +2,15 @@ import pyperclip
 
 text = pyperclip.paste()
 
-# TODO Separate lines and add stars.
 
-pyperclip.copy(text)
+# Separate lines and add stars.
+def takeTextAndMakeBulletLineByLine(text) -> str:
+    lines = text.split("\n")
+    for i in range(len(lines)):
+        lines[i] = "* " + lines[i]
+
+    output = "\n".join(lines)
+    return output
+
+
+pyperclip.copy(takeTextAndMakeBulletLineByLine(text))
